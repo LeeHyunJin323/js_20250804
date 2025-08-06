085c2bf076c891c3c2494b88a9168e20c240a12e
LeeHyunJin <skrk3042@naver.com>
2025-08-06 10:45:55 +0900

    20250806

--- work.javascript/js/clickevent.js
+++
@@ -1,8 +0,0 @@
-var link = document.querySelector("a");
-
-link.addEventListener("click",(e)=>{
-    e.preventDefault();
-    console.log("링크를 클릭했습니다.");
-});
-  
-
--- work.javascript/js/querySelector.js
+++
@@ -1,8 +0,0 @@
-    // var frame = document.querySelector("#wrap");
-    // console.log(frame);
-    var box1 = document.querySelector("#wrap .box1");
-    console.log(box1);
-
-
-  
-
